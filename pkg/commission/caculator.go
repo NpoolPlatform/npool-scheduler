fdf45ef866562c2cb2b238d3710ead0f73da9b5d
Zhao KK <kikakkz@hotmail.com>
2022-08-12 04:19:49 +0000

    Add good for commission

--- pkg/commission/caculator.go
+++ pkg/commission/caculator.go
@@ -93,10 +93,20 @@ func calculateCommission(ctx context.Context, order *orderpb.Order, payment *ord
 	for _, user := range inviters {
 		sets := settings[user]
 		for _, set := range sets {
-			if set.Start <= payment.CreateAt && (set.End == 0 || payment.CreateAt <= set.End) {
-				percent = set.Percent
-				break
+			if set.GoodID != order.GoodID {
+				continue
 			}
+
+			if set.End != 0 {
+				continue
+			}
+
+			if set.Start > payment.CreateAt || set.End < payment.CreateAt {
+				continue
+			}
+
+			subPercent = set.Percent
+			break
 		}
 
 		if percent < subPercent {
