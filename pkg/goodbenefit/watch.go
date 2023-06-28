b32e5233924dd4f5d38f838acd936066109bb818
daqiangli <dqli161@163.com>
2023-06-28 22:32:57 +0800

    every 6 clock to send

--- pkg/goodbenefit/watch.go
+++ pkg/goodbenefit/watch.go
@@ -8,13 +8,29 @@ import (
 )
 
 func Watch(ctx context.Context) {
-	ticker := time.NewTicker(30 * time.Second)
-	for {
+	hour := 6
+	minute := 0
+	second := 0
+	now := time.Now()
+	next := time.Date(now.Year(), now.Month(), now.Day(), hour, minute, second, 0, now.Location())
+	if now.After(next) {
+		next = next.Add(24 * time.Hour)
+	}
+
+	duration := next.Sub(now)
+	timer := time.NewTicker(duration)
+	defer timer.Stop()
+
+	for { //nolint
 		select {
-		case <-ticker.C:
+		case <-timer.C:
 			send(ctx, basetypes.NotifChannel_ChannelEmail)
-		case <-ctx.Done():
-			return
+			now := time.Now()
+			next := time.Date(now.Year(), now.Month(), now.Day(), hour, minute, second, 0, now.Location())
+			next = next.Add(24 * time.Hour)
+			duration = next.Sub(now)
+			timer.Stop()
+			timer = time.NewTicker(duration)
 		}
 	}
 }
