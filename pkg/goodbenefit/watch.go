2428a0f268d3656ce5dbe8c6aaa453891a941676
daqiangli <dqli161@163.com>
2023-06-29 11:29:14 +0800

    fix watch

--- pkg/goodbenefit/watch.go
+++ pkg/goodbenefit/watch.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	benefitInterval = 24 * time.Hour
-	checkInterval   = 6 * time.Hour
+	checkDelay      = 6 * time.Hour
 )
 
 func prepareInterval() {
@@ -20,9 +20,9 @@ func prepareInterval() {
 		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_INTERVAL_SECONDS"))); err == nil {
 		benefitInterval = duration
 	}
-	if duration, err := time.ParseDuration(
-		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_CHECK_INTERVAL_SECONDS"))); err == nil {
-		checkInterval = duration
+	if delay, err := time.ParseDuration(
+		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_CHECK_DELAY_SECONDS"))); err == nil {
+		checkDelay = delay
 	}
 }
 
@@ -30,7 +30,7 @@ func nextBenefitAt() time.Time {
 	now := time.Now()
 	nowSec := now.Unix()
 	benefitSeconds := int64(benefitInterval.Seconds())
-	nextSec := (nowSec+benefitSeconds)/benefitSeconds*benefitSeconds + int64(checkInterval.Seconds())
+	nextSec := (nowSec+benefitSeconds)/benefitSeconds*benefitSeconds + int64(checkDelay.Seconds())
 	return now.Add(time.Duration(nextSec-nowSec) * time.Second)
 }
 
