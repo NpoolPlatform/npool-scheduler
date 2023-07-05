f8ee860f2b6eeeeb0d71b1e4e68d68b1e663497a
daqiangli <dqli161@163.com>
2023-07-05 10:15:54 +0800

    bugfix: #1670

--- pkg/goodbenefit/send.go
+++ pkg/goodbenefit/send.go
@@ -35,7 +35,7 @@ func send(ctx context.Context, channel basetypes.NotifChannel) {
 			break
 		}
 
-		content := "<html><head><style>table {border-collapse: collapse;width: 100%;}td, th {border: 1px solid #dddddd;text-align: left;padding: 8px;}</style></head><table><tr><th>GoodID</th><th>GoodName</th><th>Amount</th><th>State</th><th>Message</th><th>BenefitDate</th></tr>" //nolint
+		content := "<html><head><style>table.notif-benefit {border-collapse: collapse;width: 100%;}#notif-good-benefit td,#notif-good-benefit th {border: 1px solid #dddddd;text-align: left;padding: 8px;}</style></head><table id='notif-good-benefit' class='notif-benefit'><tr><th>GoodID</th><th>GoodName</th><th>Amount</th><th>State</th><th>Message</th><th>BenefitDate</th></tr>" //nolint
 		benefitIDs := []string{}
 		goodIDs := []string{}
 		for _, benefit := range goodBenefits {
