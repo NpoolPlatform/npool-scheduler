12935b87eeec7a321c53fe49b7eff56c1ed9d6b3
daqiangli <dqli161@163.com>
2023-07-14 14:35:51 +0800

    refactor send

--- pkg/goodbenefit/watch.go
+++ pkg/goodbenefit/watch.go
@@ -11,8 +11,9 @@ import (
 )
 
 var (
-	benefitInterval = 24 * time.Hour
-	checkDelay      = 6 * time.Hour
+	benefitInterval  = 24 * time.Hour
+	firstCheckDelay  = 1 * time.Hour
+	secondCheckDelay = 6 * time.Hour
 )
 
 func prepareInterval() {
@@ -20,17 +21,35 @@ func prepareInterval() {
 		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_INTERVAL_SECONDS"))); err == nil {
 		benefitInterval = duration
 	}
+	if firstDelay, err := time.ParseDuration(
+		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_CHECK_FIRST_DELAY_SECONDS"))); err == nil {
+		firstCheckDelay = firstDelay
+	}
 	if delay, err := time.ParseDuration(
 		fmt.Sprintf("%vs", os.Getenv("ENV_GOOD_BENEFIT_CHECK_DELAY_SECONDS"))); err == nil {
-		checkDelay = delay
+		secondCheckDelay = delay
 	}
+}
+
+func delayBenefitAt() time.Time {
+	now := time.Now()
+	nowSec := now.Unix()
+	benefitSeconds := int64(benefitInterval.Seconds())
+	nextSec := (nowSec+benefitSeconds)/benefitSeconds*benefitSeconds + int64(firstCheckDelay.Seconds())
+	return now.Add(time.Duration(nextSec-nowSec) * time.Second)
+}
+
+func firstDelay() {
+	start := delayBenefitAt()
+	logger.Sugar().Infow("firstDelay", "startAfter", time.Until(start).Seconds(), "start", start)
+	<-time.After(time.Until(start))
 }
 
 func nextBenefitAt() time.Time {
 	now := time.Now()
 	nowSec := now.Unix()
 	benefitSeconds := int64(benefitInterval.Seconds())
-	nextSec := (nowSec+benefitSeconds)/benefitSeconds*benefitSeconds + int64(checkDelay.Seconds())
+	nextSec := (nowSec+benefitSeconds)/benefitSeconds*benefitSeconds + int64(secondCheckDelay.Seconds())
 	return now.Add(time.Duration(nextSec-nowSec) * time.Second)
 }
 
@@ -47,18 +66,26 @@ func Watch(ctx context.Context) {
 		"GoodBenefitIntervalSeconds", benefitInterval,
 	)
 
-	delay()
-
+	firstDelay()
 	send(ctx, basetypes.NotifChannel_ChannelEmail)
+	firstTickerWait := time.NewTicker(benefitInterval)
 
-	tickerWait := time.NewTicker(benefitInterval)
+	delay()
+	send(ctx, basetypes.NotifChannel_ChannelEmail)
+	secondTickerWait := time.NewTicker(benefitInterval)
 
 	for { //nolint
 		select {
-		case <-tickerWait.C:
+		case <-firstTickerWait.C:
 			logger.Sugar().Infow(
 				"Watch",
-				"State", "good benefit ticker start",
+				"State", "good benefit first ticker start",
+			)
+			send(ctx, basetypes.NotifChannel_ChannelEmail)
+		case <-secondTickerWait.C:
+			logger.Sugar().Infow(
+				"Watch",
+				"State", "good benefit second ticker start",
 			)
 			send(ctx, basetypes.NotifChannel_ChannelEmail)
 		}
