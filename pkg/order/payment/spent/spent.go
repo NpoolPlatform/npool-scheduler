3cf13d0492ea4a6a21e50e00ca7fb67a9ba54029
Zhao KK <kikakkz@hotmail.com>
2023-09-01 03:47:03 +0000

    Add payment spent process

---
+++ pkg/order/payment/spent/spent.go
@@ -0,0 +1,46 @@
+package spent
+
+import (
+	"context"
+	"time"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/npool-scheduler/pkg/base"
+	"github.com/NpoolPlatform/npool-scheduler/pkg/order/payment/spent/executor"
+	"github.com/NpoolPlatform/npool-scheduler/pkg/order/payment/spent/persistent"
+	"github.com/NpoolPlatform/npool-scheduler/pkg/order/payment/spent/sentinel"
+)
+
+const subsystem = "orderpaymentspent"
+
+var h *base.Handler
+
+func Initialize(ctx context.Context, cancel context.CancelFunc) {
+	_h, err := base.NewHandler(
+		ctx,
+		cancel,
+		base.WithSubsystem(subsystem),
+		base.WithScanInterval(time.Minute),
+		base.WithScanner(sentinel.NewSentinel()),
+		base.WithExec(executor.NewExecutor()),
+		base.WithExecutorNumber(4),
+		base.WithPersistenter(persistent.NewPersistent()),
+	)
+	if err != nil || _h == nil {
+		logger.Sugar().Errorw(
+			"Initialize",
+			"Subsystem", subsystem,
+			"Error", err,
+		)
+		return
+	}
+
+	h = _h
+	go h.Run(ctx, cancel)
+}
+
+func Finalize() {
+	if h != nil {
+		h.Finalize()
+	}
+}
