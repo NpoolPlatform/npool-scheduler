bea02f3be0aa426075b3114a0dda7b3ea8e0f8a3
lidaqiang <dqli161@163.com>
2023-09-01 15:54:28 +0800

    add msg

--- pkg/notif/notif/executor/notif.go
+++ pkg/notif/notif/executor/notif.go
@@ -12,5 +12,9 @@ type notifHandler struct {
 }
 
 func (h *notifHandler) exec(ctx context.Context) error {
+	// 获取LangID
+	// 根据Channel(Email|SMS)获取各自的模版,生成对应的SendMessage的Req
+	// 发送消息
+	// 发送消息后,需要将该消息的其它语言通知
 	return nil
 }
