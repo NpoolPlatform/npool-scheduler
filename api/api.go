fb6f14439714a74c97f5eb0e1f480a4c3371b43f
Zhao KK <kikakkz@hotmail.com>
2023-09-08 05:42:12 +0000

    Correct api implementation

--- api/api.go
+++ api/api.go
@@ -3,8 +3,6 @@ package api
 import (
 	npool "github.com/NpoolPlatform/message/npool/scheduler/mw/v1"
 
-	scheduler "github.com/NpoolPlatform/npool-scheduler/api/scheduler"
-
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
@@ -15,7 +13,6 @@ type Server struct {
 
 func Register(server grpc.ServiceRegistrar) {
 	npool.RegisterMiddlewareServer(server, &Server{})
-	scheduler.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
