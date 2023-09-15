43ea4a37a309e41d3797703a08293f489c5c0307
Zhao KK <kikakkz@hotmail.com>
2023-09-15 12:04:06 +0000

    fix deposit transfer

--- pkg/deposit/transfer/executor/account.go
+++ pkg/deposit/transfer/executor/account.go
@@ -73,18 +73,14 @@ func (h *accountHandler) checkBalance(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	if balance.Cmp(h.incoming.Sub(h.outcoming)) < 0 {
-		return nil
-	}
-	amount := h.incoming.Sub(h.outcoming)
 	reserved, err := decimal.NewFromString(h.coin.ReservedAmount)
 	if err != nil {
 		return err
 	}
-	if amount.Cmp(reserved) <= 0 {
+	if balance.Cmp(reserved) <= 0 {
 		return nil
 	}
-	h.amount = amount.Sub(reserved)
+	h.amount = balance.Sub(reserved)
 	return nil
 }
 
