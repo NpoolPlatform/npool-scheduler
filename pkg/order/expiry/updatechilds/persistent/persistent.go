b8b2667ffa47e7fbbb535afec4dfc43b851c21c0
Zhao KK <kikakkz@hotmail.com>
2024-01-06 08:27:16 +0000

    Add child expired parent

--- pkg/order/expiry/updatechilds/persistent/persistent.go
+++ pkg/order/expiry/updatechilds/persistent/persistent.go
@@ -33,10 +33,11 @@ func (p *handler) Update(ctx context.Context, order interface{}, notif, done cha
 			OrderState: &orderState,
 		},
 	}
+	orderState1 := ordertypes.OrderState_OrderStateChildExpiredByParent
 	for _, child := range _order.ChildOrders {
 		reqs = append(reqs, &ordermwpb.OrderReq{
 			ID:         &child.ID,
-			OrderState: &orderState,
+			OrderState: &orderState1,
 		})
 	}
 	if _, err := ordermwcli.UpdateOrders(ctx, reqs); err != nil {
