20d3607757721eb374d9092e47720193dc982b63
Zhao KK <kikakkz@hotmail.com>
2024-01-06 08:33:30 +0000

    Add cancel child canceled parent

---
+++ pkg/order/cancel/childcanceledparent/types/types.go
@@ -0,0 +1,11 @@
+package types
+
+import (
+	ordermwpb "github.com/NpoolPlatform/message/npool/order/mw/v1/order"
+)
+
+type PersistentOrder struct {
+	*ordermwpb.Order
+	AppGoodStockID string
+	Error          error
+}
