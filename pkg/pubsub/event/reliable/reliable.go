6e8c6a4a7f5ae1cda9361314c94b9f0f32cb3112
jakys <jakysing@163.com>
2024-05-17 07:44:32 +0000

    Fix reliable

--- pkg/pubsub/event/reliable/reliable.go
+++ pkg/pubsub/event/reliable/reliable.go
@@ -16,7 +16,9 @@ import (
 	eventmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/event"
 	taskusermwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/task/user"
 	usercredithistorymwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/user/credit/history"
+	userrewardmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/user/reward"
 	statementmwpb "github.com/NpoolPlatform/message/npool/ledger/mw/v2/ledger/statement"
+	dtm1 "github.com/NpoolPlatform/npool-scheduler/pkg/dtm"
 	"github.com/dtm-labs/dtm/client/dtmcli/dtmimp"
 
 	"github.com/google/uuid"
@@ -29,17 +31,34 @@ type calculateHandler struct {
 func (h *calculateHandler) withCreateCredit(dispose *dtmcli.SagaDispose) {
 	for _, credit := range h.req.Credits {
 		id := uuid.NewString()
+		req := &usercredithistorymwpb.UserCreditHistoryReq{
+			EntID:   &id,
+			AppID:   &credit.AppID,
+			UserID:  &credit.UserID,
+			TaskID:  &h.req.TaskID,
+			EventID: &h.req.EventID,
+			Credits: &credit.Credits,
+		}
+		rewardReq := &userrewardmwpb.UserRewardReq{
+			EntID:         &id,
+			AppID:         &h.req.AppID,
+			UserID:        &h.req.UserID,
+			ActionCredits: &credit.Credits,
+		}
 		dispose.Add(
 			inspiremwsvcname.ServiceDomain,
 			"inspire.middleware.user.credit.history.v1.Middleware/CreateUserCreditHistory",
 			"inspire.middleware.user.credit.history.v1.Middleware/DeleteUserCreditHistory",
-			&usercredithistorymwpb.UserCreditHistoryReq{
-				EntID:   &id,
-				AppID:   &credit.AppID,
-				UserID:  &credit.UserID,
-				TaskID:  &h.req.TaskID,
-				EventID: &h.req.EventID,
-				Credits: &credit.Credits,
+			&usercredithistorymwpb.CreateUserCreditHistoryRequest{
+				Info: req,
+			},
+		)
+		dispose.Add(
+			inspiremwsvcname.ServiceDomain,
+			"inspire.middleware.user.reward.v1.Middleware/AddUserReward",
+			"inspire.middleware.user.reward.v1.Middleware/SubUserReward",
+			&userrewardmwpb.AddUserRewardRequest{
+				Info: rewardReq,
 			},
 		)
 	}
@@ -49,20 +68,22 @@ func (h *calculateHandler) withCreateCoin(dispose *dtmcli.SagaDispose) {
 	if h.req.CoinRewards == nil {
 		return
 	}
-
 	for _, coin := range h.req.CoinRewards {
 		id := uuid.NewString()
+		req := &coinallocatedmwpb.CoinAllocatedReq{
+			EntID:        &id,
+			AppID:        &coin.AppID,
+			UserID:       &coin.UserID,
+			CoinConfigID: &coin.CoinConfigID,
+			CoinTypeID:   &coin.CoinTypeID,
+			Value:        &coin.CoinRewards,
+		}
 		dispose.Add(
 			inspiremwsvcname.ServiceDomain,
 			"inspire.middleware.coin.allocated.v1.Middleware/CreateCoinAllocated",
 			"inspire.middleware.coin.allocated.v1.Middleware/DeleteCoinAllocated",
-			&coinallocatedmwpb.CoinAllocatedReq{
-				EntID:        &id,
-				AppID:        &coin.AppID,
-				UserID:       &coin.UserID,
-				CoinConfigID: &coin.CoinConfigID,
-				CoinTypeID:   &coin.CoinTypeID,
-				Value:        &coin.CoinRewards,
+			&coinallocatedmwpb.CreateCoinAllocatedRequest{
+				Info: req,
 			},
 		)
 	}
@@ -74,9 +95,8 @@ func (h *calculateHandler) withCreateLedgerStatements(dispose *dtmcli.SagaDispos
 	}
 	rollback := true
 	ioType := ledgertypes.IOType_Incoming
-	ioSubType := ledgertypes.IOSubType_EventReward
+	ioSubType := ledgertypes.IOSubType_SimulateMiningBenefit
 	reqs := []*statementmwpb.StatementReq{}
-
 	for _, coin := range h.req.CoinRewards {
 		id := uuid.NewString()
 		now := uint32(time.Now().Unix())
@@ -85,6 +105,7 @@ func (h *calculateHandler) withCreateLedgerStatements(dispose *dtmcli.SagaDispos
 			ev.EntID,
 			ev.EventType,
 		)
+
 		reqs = append(reqs, &statementmwpb.StatementReq{
 			EntID:      &id,
 			AppID:      &coin.AppID,
@@ -113,18 +134,24 @@ func (h *calculateHandler) WithCreateTaskUser(dispose *dtmcli.SagaDispose, ev *e
 	id := uuid.NewString()
 	taskState := inspiretypes.TaskState_Done
 	rewardState := inspiretypes.RewardState_Issued
+	rewardInfo := ""
+
+	req := &taskusermwpb.TaskUserReq{
+		EntID:       &id,
+		AppID:       &h.req.AppID,
+		UserID:      &h.req.UserID,
+		TaskID:      &h.req.TaskID,
+		EventID:     &h.req.EventID,
+		TaskState:   &taskState,
+		RewardState: &rewardState,
+		RewardInfo:  &rewardInfo,
+	}
 	dispose.Add(
 		inspiremwsvcname.ServiceDomain,
 		"inspire.middleware.task.user.v1.Middleware/CreateTaskUser",
 		"inspire.middleware.task.user.v1.Middleware/DeleteTaskUser",
-		&taskusermwpb.TaskUserReq{
-			EntID:       &id,
-			AppID:       &h.req.AppID,
-			UserID:      &h.req.UserID,
-			TaskID:      &h.req.TaskID,
-			EventID:     &h.req.EventID,
-			TaskState:   &taskState,
-			RewardState: &rewardState,
+		&taskusermwpb.CreateTaskUserRequest{
+			Info: req,
 		},
 	)
 }
@@ -157,18 +184,21 @@ func Apply(ctx context.Context, req interface{}) error {
 		RequestTimeout: timeoutSeconds,
 		TimeoutToFail:  timeoutSeconds,
 	})
-
 	// create credit
 	handler.withCreateCredit(sagaDispose)
 
 	// create coins
 	handler.withCreateCoin(sagaDispose)
 
-	// create ledger
+	// // create ledger
 	handler.withCreateLedgerStatements(sagaDispose, ev)
 
-	// create task user
+	// // create task user
 	handler.WithCreateTaskUser(sagaDispose, ev)
+
+	if err := dtm1.Do(ctx, sagaDispose); err != nil {
+		return err
+	}
 
 	return nil
 }
--- pkg/pubsub/event/unreliable/unreliable.go
+++ pkg/pubsub/event/unreliable/unreliable.go
@@ -7,11 +7,10 @@ import (
 
 	dtmcli "github.com/NpoolPlatform/dtm-cluster/pkg/dtm"
 	inspiremwsvcname "github.com/NpoolPlatform/inspire-middleware/pkg/servicename"
-	inspiretypes "github.com/NpoolPlatform/message/npool/basetypes/inspire/v1"
 	couponallocatedmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/coupon/allocated"
 	eventmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/event"
-	taskusermwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/task/user"
 	userrewardmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/user/reward"
+	dtm1 "github.com/NpoolPlatform/npool-scheduler/pkg/dtm"
 	"github.com/dtm-labs/dtm/client/dtmcli/dtmimp"
 
 	"github.com/shopspring/decimal"
@@ -35,7 +34,7 @@ func (h *calculateHandler) withCreateCoupon(dispose *dtmcli.SagaDispose) {
 		}
 		dispose.Add(
 			inspiremwsvcname.ServiceDomain,
-			"inspire.middleware.coupon.allocated.v1.Middleware/CreateCoupon",
+			"inspire.middleware.coupon.allocated.v1.Middleware/CreateDirectCoupon",
 			"inspire.middleware.coupon.allocated.v1.Middleware/DeleteCoupon",
 			&couponallocatedmwpb.CreateCouponRequest{
 				Info: req,
@@ -44,43 +43,26 @@ func (h *calculateHandler) withCreateCoupon(dispose *dtmcli.SagaDispose) {
 	}
 }
 
-func (h *calculateHandler) WithCreateTaskUser(dispose *dtmcli.SagaDispose) {
-	id := uuid.NewString()
-	taskState := inspiretypes.TaskState_Done
-	rewardState := inspiretypes.RewardState_Issued
-	dispose.Add(
-		inspiremwsvcname.ServiceDomain,
-		"inspire.middleware.task.user.v1.Middleware/CreateTaskUser",
-		"inspire.middleware.task.user.v1.Middleware/DeleteTaskUser",
-		&taskusermwpb.TaskUserReq{
-			EntID:       &id,
-			AppID:       &h.req.AppID,
-			UserID:      &h.req.UserID,
-			TaskID:      &h.req.TaskID,
-			EventID:     &h.req.EventID,
-			TaskState:   &taskState,
-			RewardState: &rewardState,
-		},
-	)
-}
-
-func (h *calculateHandler) WithCreateUserReward(dispose *dtmcli.SagaDispose, ev *eventmwpb.Event) {
+func (h *calculateHandler) WithCreateUserReward(dispose *dtmcli.SagaDispose) {
 	id := uuid.NewString()
 	for _, coupon := range h.req.Coupons {
 		couponCashableAmount := decimal.NewFromInt(0).String()
 		if coupon.Cashable {
 			couponCashableAmount = coupon.Denomination
 		}
+		rewardReq := &userrewardmwpb.UserRewardReq{
+			EntID:                &id,
+			AppID:                &h.req.AppID,
+			UserID:               &h.req.UserID,
+			CouponAmount:         &coupon.Denomination,
+			CouponCashableAmount: &couponCashableAmount,
+		}
 		dispose.Add(
 			inspiremwsvcname.ServiceDomain,
-			"inspire.middleware.user.reward.v1.Middleware/CreateUserReward",
-			"inspire.middleware.user.reward.v1.Middleware/DeleteUserReward",
-			&userrewardmwpb.UserRewardReq{
-				EntID:                &id,
-				AppID:                &h.req.AppID,
-				UserID:               &h.req.UserID,
-				CouponAmount:         &coupon.Denomination,
-				CouponCashableAmount: &couponCashableAmount,
+			"inspire.middleware.user.reward.v1.Middleware/AddUserReward",
+			"inspire.middleware.user.reward.v1.Middleware/SubUserReward",
+			&userrewardmwpb.AddUserRewardRequest{
+				Info: rewardReq,
 			},
 		)
 	}
@@ -95,6 +77,7 @@ func Prepare(body string) (interface{}, error) {
 }
 
 func Apply(ctx context.Context, req interface{}) error {
+	fmt.Println("apply unreliable reward")
 	in, ok := req.(*eventmwpb.RewardUnReliableRequest)
 	if !ok {
 		return fmt.Errorf("invalid request in apply")
@@ -114,7 +97,11 @@ func Apply(ctx context.Context, req interface{}) error {
 	handler.withCreateCoupon(sagaDispose)
 
 	// create task user
-	handler.WithCreateTaskUser(sagaDispose)
+	handler.WithCreateUserReward(sagaDispose)
+
+	if err := dtm1.Do(ctx, sagaDispose); err != nil {
+		return err
+	}
 
 	return nil
 }
