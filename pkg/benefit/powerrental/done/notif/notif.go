7dfe9de06d2eb807922817ad75b501ed140f4caa
Zhao KK <kikakkz@hotmail.com>
2024-07-09 12:49:49 +0000

    Add amount and message to notif

--- pkg/benefit/powerrental/done/notif/notif.go
+++ pkg/benefit/powerrental/done/notif/notif.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/pubsub"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	goodcoinrewardmwpb "github.com/NpoolPlatform/message/npool/good/mw/v1/good/coin/reward"
 	notifbenefitmwpb "github.com/NpoolPlatform/message/npool/notif/mw/v1/notif/goodbenefit"
 	basenotif "github.com/NpoolPlatform/npool-scheduler/pkg/base/notif"
 	retry1 "github.com/NpoolPlatform/npool-scheduler/pkg/base/retry"
@@ -23,15 +24,33 @@ func (p *handler) notifyGoodBenefit(good *types.PersistentGood) error {
 	return pubsub.WithPublisher(func(publisher *pubsub.Publisher) error {
 		now := uint32(time.Now().Unix())
 		result := basetypes.Result_Success
-		req := &notifbenefitmwpb.GoodBenefitReq{
-			GoodID:      &good.GoodID,
-			GoodType:    &good.GoodType,
-			GoodName:    &good.Name,
-			State:       &result,
-			BenefitDate: &now,
-		}
-		for _, goodCoin := range good.GoodCoins {
-			req.CoinTypeID = &goodCoin.CoinTypeID
+
+		for _, reward := range good.CoinNextRewards {
+			_reward := func() *goodcoinrewardmwpb.RewardInfo {
+				for _, r := range good.Rewards {
+					if r.CoinTypeID == reward.CoinTypeID {
+						return r
+					}
+				}
+				return nil
+			}()
+
+			if _reward == nil {
+				return fmt.Errorf("invalid reward")
+			}
+
+			req := &notifbenefitmwpb.GoodBenefitReq{
+				GoodID:      &good.GoodID,
+				GoodType:    &good.GoodType,
+				GoodName:    &good.Name,
+				Amount:      &_reward.LastRewardAmount,
+				TxID:        &_reward.RewardTID,
+				State:       &result,
+				BenefitDate: &now,
+				CoinTypeID:  &reward.CoinTypeID,
+				Message:     &reward.BenefitMessage,
+			}
+
 			if err := publisher.Update(
 				basetypes.MsgID_CreateGoodBenefitReq.String(),
 				nil,
