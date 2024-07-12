b142037be96df33fdd7ec2fcb93d0c7a5bc1db05
jakys <jakysing@163.com>
2024-07-12 17:34:40 +0800

    Add withdrawalcompleted reward event

--- pkg/withdraw/successful/presuccessful/persistent/persistent.go
+++ pkg/withdraw/successful/presuccessful/persistent/persistent.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	"github.com/NpoolPlatform/go-service-framework/pkg/pubsub"
 	withdrawmwcli "github.com/NpoolPlatform/ledger-middleware/pkg/client/withdraw"
 	ledgertypes "github.com/NpoolPlatform/message/npool/basetypes/ledger/v1"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	eventmwpb "github.com/NpoolPlatform/message/npool/inspire/mw/v1/event"
 	withdrawmwpb "github.com/NpoolPlatform/message/npool/ledger/mw/v2/withdraw"
 	asyncfeed "github.com/NpoolPlatform/npool-scheduler/pkg/base/asyncfeed"
 	basepersistent "github.com/NpoolPlatform/npool-scheduler/pkg/base/persistent"
@@ -18,7 +22,32 @@ func NewPersistent() basepersistent.Persistenter {
 	return &handler{}
 }
 
-func (p *handler) Update(ctx context.Context, withdraw interface{}, notif, done chan interface{}) error {
+func (h *handler) rewardWithdraw(_withdraw *types.PersistentWithdraw) {
+	if err := pubsub.WithPublisher(func(publisher *pubsub.Publisher) error {
+		req := &eventmwpb.CalcluateEventRewardsRequest{
+			AppID:       _withdraw.AppID,
+			UserID:      _withdraw.UserID,
+			EventType:   basetypes.UsedFor_WithdrawalCompleted,
+			Consecutive: 1,
+		}
+		return publisher.Update(
+			basetypes.MsgID_CalculateEventRewardReq.String(),
+			nil,
+			nil,
+			nil,
+			req,
+		)
+	}); err != nil {
+		logger.Sugar().Errorw(
+			"rewardWithdraw",
+			"AppID", _withdraw.AppID,
+			"UserID", _withdraw.UserID,
+			"Error", err,
+		)
+	}
+}
+
+func (h *handler) Update(ctx context.Context, withdraw interface{}, notif, done chan interface{}) error {
 	_withdraw, ok := withdraw.(*types.PersistentWithdraw)
 	if !ok {
 		return fmt.Errorf("invalid withdraw")
@@ -33,6 +62,8 @@ func (p *handler) Update(ctx context.Context, withdraw interface{}, notif, done 
 	}); err != nil {
 		return err
 	}
+
+	h.rewardWithdraw(_withdraw)
 
 	return nil
 }
