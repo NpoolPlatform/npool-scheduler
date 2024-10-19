8997f1c2a7c3618dbc08a738e80d7800f108e094
Zhao KK <kikakkz@hotmail.com>
2024-10-19 10:27:22 +0000

    update scan interval

--- pkg/deposit/user/user.go
+++ pkg/deposit/user/user.go
@@ -23,7 +23,7 @@ func Initialize(ctx context.Context, cancel context.CancelFunc, running *sync.Ma
 		ctx,
 		cancel,
 		base.WithSubsystem(subsystem),
-		base.WithScanInterval(30*time.Second),
+		base.WithScanInterval(5*time.Minute),
 		base.WithScanner(sentinel.NewSentinel()),
 		base.WithNotify(notif.NewNotif()),
 		base.WithExec(executor.NewExecutor()),
