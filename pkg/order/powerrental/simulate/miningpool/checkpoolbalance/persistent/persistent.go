0be47b5d406d266d3735aacc6ba66ccc494112f1
Geapefurit <670884108@qq.com>
2024-10-22 08:03:33 +0000

    deal other state for simulate order

---
+++ pkg/order/powerrental/simulate/miningpool/checkpoolbalance/persistent/persistent.go
@@ -0,0 +1,34 @@
+package persistent
+
+import (
+	"context"
+	"fmt"
+
+	ordertypes "github.com/NpoolPlatform/message/npool/basetypes/order/v1"
+	powerrentalordermwpb "github.com/NpoolPlatform/message/npool/order/mw/v1/powerrental"
+	asyncfeed "github.com/NpoolPlatform/npool-scheduler/pkg/base/asyncfeed"
+	basepersistent "github.com/NpoolPlatform/npool-scheduler/pkg/base/persistent"
+	types "github.com/NpoolPlatform/npool-scheduler/pkg/order/powerrental/simulate/miningpool/checkpoolbalance/types"
+	powerrentalordermwcli "github.com/NpoolPlatform/order-middleware/pkg/client/powerrental"
+)
+
+type handler struct{}
+
+func NewPersistent() basepersistent.Persistenter {
+	return &handler{}
+}
+
+func (p *handler) Update(ctx context.Context, order interface{}, notif, done chan interface{}) error {
+	_order, ok := order.(*types.PersistentOrder)
+	if !ok {
+		return fmt.Errorf("invalid order")
+	}
+
+	defer asyncfeed.AsyncFeed(ctx, _order, done)
+
+	state := ordertypes.OrderState_OrderStateSetRevenueAddress
+	return powerrentalordermwcli.UpdatePowerRentalOrder(ctx, &powerrentalordermwpb.PowerRentalOrderReq{
+		ID:         &_order.ID,
+		OrderState: &state,
+	})
+}
